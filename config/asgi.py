d8940a3948d2dfd55ba49e8dc92af6d769dd571a
Hruaia <hruaitluangasailo20@gmail.com>
2026-04-08 21:35:50 +0530

    fix

--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,18 @@
-from accounts.routing import websocket_urlpatterns as accounts_ws
-from forum.routing import websocket_urlpatterns as forum_ws
-import sessions_app.routing
-import livestream.routing
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 django_asgi_app = get_asgi_application()
 
+import livestream.routing
+import sessions_app.routing
+from forum.routing import websocket_urlpatterns as forum_ws
+from accounts.routing import websocket_urlpatterns as accounts_ws
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
